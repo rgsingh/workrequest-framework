d57705f392dd4edec8958bd65e57c5912cdc33ea
Rai Singh <raigsingh@gmail.com>
2012-11-12 20:46:53 -0500

    Added webapp for testing MVC with JAXB and with JSP views.

---
+++ workqueue-frontend-webapp/src/main/java/com/rgsinfotech/main/controller/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.rgsinfotech.main.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class WelcomeController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String welcome(Model model) {
+		Date today = new Date();
+		model.addAttribute("today", today);
+		return "welcome";
+	}
+}
--- workqueue-model/src/test/java/com/rgsinfotech/model/AppTest.java
+++ workqueue-model/src/test/java/com/rgsinfotech/model/AppTest.java
@@ -1,4 +1,4 @@
-package com.rgsinfotech.xmldatabinder;
+package com.rgsinfotech.model;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
